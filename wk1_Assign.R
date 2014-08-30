01cb60dccf004ad8924e5db3642b8021f926343b
jordanericks <jordanericks@gmail.com>
2014-08-29 23:07:03 -0500

    Week 1 assignment_jordan

--- wk1_Assign.R
+++ wk1_Assign.R
@@ -1,6 +1,10 @@
 # Week 1 Assignment
 # Jordan Erickson
 
+# 1. R version 3.0.2, RStudio version 0.98.501
+# 2. PostgreSQL version 9.3.5 (pgAdmin version 1.18.1)
+# 3. ...
+
 install.packages("DMwR")
 require(DMwR)
 
